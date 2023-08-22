4b227e5332279a8efdb0318c1e0dcb6307af414c
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-08-22 21:41:47 +0300

    feat : Route for verified users to access a secret route

---
+++ jwt-passport-node-script/routes/secure-routes.js
@@ -0,0 +1,10 @@
+import Express from "express";
+export const router = Express.Router();
+
+router.get("/profile", (req, res, next) => {
+  res.json({
+    message: "You made it to the secure route",
+    user: req.user,
+    token: req.query.secret_token,
+  });
+});
