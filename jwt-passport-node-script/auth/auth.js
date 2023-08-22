2eb32c7a60a4426a84ad8252dd0b7fd3bdb3afd2
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-08-22 20:53:35 +0300

    feat : Middleware to handle user login

--- jwt-passport-node-script/auth/auth.js
+++ jwt-passport-node-script/auth/auth.js
@@ -23,3 +23,34 @@ passport.use(
         }
     }
 )
+
+//Find user associated with email
+passport.use(
+    'login',
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+
+        async (email, password, done) =>{
+            try{
+                const user = await UserModel.findOne({email})
+
+                if(!user){
+                    return done(null, false, {message: "User not found"})
+                }
+
+                const validate = await user.isValidPassword(password)
+
+                if(!validate){
+                    return done(null, false, { message : "Wrong password"})
+                }
+
+                return done(null, user, {message: "Logged in successfully"})
+            } catch(error){
+                return done(error)
+            }
+        },
+    )
+)
