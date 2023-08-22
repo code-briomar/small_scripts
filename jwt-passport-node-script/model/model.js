e57d91041277a388be5f6be6af9ffc0e6c323101
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-08-22 13:43:18 +0300

    feat : Validate user credentials during login and comments to the password hashing pre-hook

--- jwt-passport-node-script/model/model.js
+++ jwt-passport-node-script/model/model.js
@@ -16,14 +16,31 @@ const UserSchema = new Schema({
     }
 });
 
-UserSchema.pre(
+UserSchema.pre( //pre-hook
+//Before saving the info in the db, this function will be called
     'save',
+    //this - Current document to be saved
     async (next) =>{
         const user = this;
+        //10 rep the salt cost - Iterations ran for hashing.
+        //The higher the no. the higher the iterations hence 
+        //the more computation required which may impact app
+        //performance - but pwd will be more secure.
         const hash = await bcrypt.hash(this.password, 10)
-
+        //Hash the password
         this.password = hash;
         next()
     }
 )
+
+//Validate user credentials when logging into the system
+UserSchema.methods.isValidPassword = async (password) => {
+    const user = this;
+    //Hash the pwd sent and compare it with one stored in the db
+    const compare = await bcrypt.compare(password, user.password);
+
+    return compare
+}
+
+
 export const UserModel = mongoose.model('user', UserSchema)
