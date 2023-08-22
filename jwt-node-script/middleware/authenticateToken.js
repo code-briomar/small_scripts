5052a1145e95de6b04b1b6c7b7671df98bcd07a9
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-08-22 10:53:02 +0300

    docs : Comment to explain how token is retrieved from the header

--- jwt-node-script/middleware/authenticateToken.js
+++ jwt-node-script/middleware/authenticateToken.js
@@ -4,6 +4,8 @@ import { KEYS } from "../config/keys.js"
 export const authenticateToken = (req, res, next) =>{
     const authHeader = req.headers["authorization"]
 
+    //If authHeader exists, split it and place values in an array 
+    //and the value in the second position from the start, is the token
     const token = authHeader && authHeader.split(" ")[1];
 
     if(token == null) return res.sendStatus(401);
