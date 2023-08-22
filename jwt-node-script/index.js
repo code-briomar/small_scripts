5742789c0a5c089c847ea374a87c1a98a1120da9
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-08-22 08:26:13 +0300

    feat : Route to generate access token for user

--- jwt-node-script/index.js
+++ jwt-node-script/index.js
@@ -1,9 +1,16 @@
 import Express  from "express"
-import jwt from "jsonwebtoken"
 import { KEYS } from "./config/keys.js";
-
+import { generateAccessToken } from "./functions/generateAccessToken.js";
 
 const app = Express();
+
+app.use(Express.json())
+
+app.post("/jwt-api/login",(req, res)=>{
+    const token = generateAccessToken({username: req.body.username})
+    res.json(token)
+})
+
 
 app.listen(KEYS.PORT,()=>{
     console.log(`Server running on port ${KEYS.PORT}`);
